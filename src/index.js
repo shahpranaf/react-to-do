ec81ee7d2ee3824d00621dff097642221bd4c796
Pranav Shah <Pranav.Shah@katerra.com>
2021-03-28 17:22:36 +0530

    Added Readme and fixed errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Todo from './Todo';
 
